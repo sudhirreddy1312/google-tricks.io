0e54b5737f33f4a7505835442f34041b3738c9e5
sudhirreddy1312 <60067021+sudhirreddy1312@users.noreply.github.com>
2020-04-28 17:35:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+setTimeout(function note(){
+alert("NOTE Spare Some time To Read the Trick Info Fully, If You Really Like The Website And The Tricks Please Sign Up For Me By Clicking On Sign Up On The Top");
+},10000);
+
+
+
+	
