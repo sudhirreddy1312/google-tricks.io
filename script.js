f9e0f1c0a71e593de4eb2c0f6a43812f0f7c2744
sudhirreddy1312 <60067021+sudhirreddy1312@users.noreply.github.com>
2022-12-21 13:21:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 setTimeout(function note(){
 alert("NOTE Spare Some time To Read the Trick Info Fully, If You Really Like The Website And The Tricks Please Sign Up For Me By Clicking On Sign Up On The Top");
-},10000);
+},5000);
 
 
 
